45d28b4d172e82296797de769de27d94a6313a9d
gustavoesucri <gustavo.esucri@gmail.com>
2025-04-13 17:29:39 -0300

    Calculadora
    Versão simplificada da calculadora. Sem CSS completo, sem validação...

--- src/calculadora/Calculadora.jsx
+++ src/calculadora/Calculadora.jsx
@@ -2,58 +2,75 @@ import { useState } from "react";
 
 export default function Calculadora(){
 
+    const [message, setMessage] = useState("");
 
-    let [message, setMessage] = useState('');
-    let [n1, setN1] = useState('');
-    let [n2, setN2] = useState(''); 
-
-    function setValues(){
-        setN1(parseFloat(document.getElementById("num1").value))
-        setN2(parseFloat(document.getElementById("num2").value))
-    }
-
-    function soma(){
-        setValues()
-        return setMessage(`A soma de ${n1} + ${n2} é ${n1+n2}`)
+    function soma(n1,n2){
+        return setMessage(`A soma de ${n1} + ${n2} é = ${n1+n2}`)
 }
 
+    function subtracao(n1,n2){
+        return setMessage(`A substração de ${n1} - ${n2} é = ${n1-n2}`)
+    }
 
+    function multiplicacao(n1,n2){
+        return setMessage(`A multiplicação de ${n1} * ${n2} é = ${n1*n2}`)
+    }
+
+    function divisao(n1,n2){
+        return setMessage(`A divisão de ${n1} / ${n2} é = ${n1/n2}`)
+    }
+
+    function operacao(){
+        const n1 = Number(document.getElementById("num1").value);
+        const n2 = Number(document.getElementById("num2").value);
+        const oper = document.getElementById("operacao").value
+        switch (oper) {
+            case "1":
+                soma(n1,n2);
+                break;
+            case "2":
+                subtracao(n1,n2);
+                break;
+            case "3":
+                multiplicacao(n1,n2);
+                break;
+            case "4":
+                divisao(n1,n2);
+                break;
+            default:
+                setMessage("Selecione uma operação válida.");
+
+        }
+    }
+
+    function limpaCampos(){
+        setMessage("");
+        document.getElementById("num1").value = ""
+        document.getElementById("num2").value = ""
+    }
 
      return <section>
         <p>Digite os números e selecione uma opção para ver o resultado:</p>
                  <br/>
-                 <input id="num1" type="number" />
-                 <input id="num2" type="number" />
-                 <br/>
-                 <button onClick={soma}>Soma</button>
-                 <button>Subtração</button>
-                 <button>Multiplicação</button>                
-                 <button>Divisão</button>
+                 <input id="num1" type="text" />
+                 <input id="num2" type="text" />
+                 <select id="operacao">
+                 <option value="1">Soma</option>
+                 <option value="2">Subtração</option>
+                 <option value="3">Multiplicação</option>
+                 <option value="4">Divisão</option>
+                 </select>
+                 <br/><br/>
+
+                 <button onClick={operacao}>Calcular</button>
+                 <button onClick={limpaCampos}>Limpar</button>
                  {message && <p>{message}</p>}
             </section>
 
 }
 
-{/* //             <p>Selecione uma opção e digite os números:</p>
-//             <div>
-//                 <button onClick={setValues()}>Seta Valores</button>
-//                 <button onClick={soma(n1,n2)}>Soma</button>
-//                 <button>Subtração</button>
-//                 <button>Multiplicação</button>
-//                 <button>Divisão</button>
-//             </div>
-//             <div>
-//             <input id="num1" type="number" value={n1}/>
-//             <input id="num2" type="number" value={n2}/>
-//             </div>
-//             <div>
-//             {message && <p>{message}</p>}
-//             </div>
-//             </section> */}
-
-
-
 // Crie uma página web que faça operações básicas de soma, subtração, multiplicação e divisão e que
 // contenha dois campos para informar os valores e uma opção para selecionar a operação. Ainda deve
 // conter dois botões: Calcular, que realizará a operação, apresentando o resultado em um parágrafo e
 // Limpar que apagará os valores dos campos e o resultado.
+
